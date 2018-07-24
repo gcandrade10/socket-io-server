f183ac8378f506963155ecbf81a707bb7491ec59
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-07-23 22:37:22 -0500

    cors enable x2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
 const path = require('path');
+var cors = require('cors')
 
 let clients=[];
 let clientsInfo=[];
@@ -15,24 +16,7 @@ let cerradas=0;
 let abiertas=0;
 let activeGame=false;
 
-app.use(function (req, res, next) {
-
-   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', '*');
-
-   // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-   // Request headers you wish to allow
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.setHeader('Access-Control-Allow-Credentials', true);
-
-   // Pass to next layer of middleware
-   next();
-});
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'socket-io-client/build')));
 
