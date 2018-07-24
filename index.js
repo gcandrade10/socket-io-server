6f741f42e48e7ba6ccfeb4773587f4c3bd400a08
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-07-23 22:49:55 -0500

    deploy x3

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ let activeGame=false;
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'socket-io-client/build')));
 
 app.get('*', (req, res) => {
