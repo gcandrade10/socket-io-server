c938b975765cb5a22c8c99ff34bb6d2017fbd9e1
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-07-23 22:56:36 -0500

    deploy x5

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+})
+
 app.use(express.static(path.join(__dirname, 'socket-io-client/build')));
 
 app.get('*', (req, res) => {
