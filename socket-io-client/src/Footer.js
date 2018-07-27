2736fb0f0a6e60702766be852ccb8f09371dd38d
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-07-26 20:55:33 -0500

    fixed estudiantes misspeling

--- socket-io-client/src/Footer.js
+++ socket-io-client/src/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 		return (
 			<div className="footer">
 	        	<h4>&reg; <strong>Encuesta interactiva</strong></h4>
-	            <p className="details">by Sergio Pardo y Germán Andrade | Estuadiantes de Ingeniería de Sistemas | Universidad de los andes <strong>2018</strong></p>      
+	            <p className="details">by Sergio Pardo y Germán Andrade | Estudiantes de Ingeniería de Sistemas | Universidad de los andes <strong>2018</strong></p>      
 	        </div>
 		);
 	}
