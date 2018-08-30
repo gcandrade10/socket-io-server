3e7b2482fcec9269f351905562bc7f43790706f3
German Andrade <gc.andrade10@uniandes.edu.co>
2018-08-30 18:32:45 -0500

    Fixed timer bug!

--- socket-io-client/src/Client.js
+++ socket-io-client/src/Client.js
@@ -11,8 +11,8 @@ class Client extends Component {
     super();
     this.state = {
       response: false,
-      //endpoint: process.env.ENDPOINT || "127.0.0.1:4001",
-      endpoint: "https://afternoon-depths-66584.herokuapp.com",
+      endpoint: process.env.ENDPOINT || "127.0.0.1:4001",
+      //endpoint: "https://afternoon-depths-66584.herokuapp.com",
       nombre: "",
       codigo: "",
       step:0
@@ -102,6 +102,7 @@ class Client extends Component {
     });
 
     this.socket.on("question2", data => {
+      timerInstance = new Timer();
       timerInstance.start();
       this.startTimer();
       console.log(data);
@@ -109,6 +110,7 @@ class Client extends Component {
     });
 
     this.socket.on("question3", data => {
+      timerInstance = new Timer();
       timerInstance.start();
       this.startTimer();
       console.log(data);
